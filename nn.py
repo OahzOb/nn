34e6deabbc5be659dbf593c0e5739977bca944e1
X99_win <X99_win.com>
2025-10-04 17:41:11 +0800

    1003

--- nn.py
+++ nn.py
@@ -4,28 +4,54 @@ random.seed(42)
 
 
 class Neuron:
-    def __init__(self, weight: float, bias: float):
+    def __init__(self, weight: list[float, ], bias: float):
         self.weight = weight
         self.bias = bias
 
-    def forward(self, x: np.ndarray) -> np.ndarray:
-        y = self.weight @ x + self.bias
-        return y
+    def forward(self, x: list[float, ]) -> list[float]:
+        y = 0
+        for w, x0 in zip(self.weight, x):
+            y += w * x0
+        y += self.bias
+        return [y]
 
 
 class Layer:
-    def __init__(self, input_size: int, output_size):
+    def __init__(self, input_size: int, output_size: int):
         neurons = list()
-        for _ in range(num_neuron):
-            weight = random.random()
-            bias = random.random()
+        for _ in range(output_size):
+            weight = [random.uniform(-1.0, 1.0) for _ in range(input_size)]
+            bias = random.uniform(-1.0, 1.0)
             neuron = Neuron(weight=weight, bias=bias)
             neurons.append(neuron)
         self.neurons = neurons
-
-    def forward(self, x: float) -> :
+        self.input_size = input_size
+        self.output_size = output_size
+    def forward(self, x: list[float, ]) -> list[float, ]:
+        if len(x) != self.input_size:
+            raise ValueError("Dims not matched!")
+        output = list()
+        for neuron in self.neurons:
+            y = neuron.forward(x)
+            output += y # output.append(y[0])
+        # ReLU
+        output_activated = list()
+        for y in output:
+            if y > 0:
+                output_activated.append(y)
+            else:
+                output_activated.append(0)
+        # Sigmoid
+        output_activated = list()
+        for y in output:
+            y_new = 1 / (1 + np.exp(-y))
+            output_activated.append(float(y_new))
+        return output_activated
 
 
 if __name__ == '__main__':
-    n1 = Neuron(1, 1)
-    print(n1.forward(10))
+    layer = Layer(input_size=10, output_size=20)
+    x = [1] * 10
+    y = layer.forward(x=x)
+    print(y)
+    print('len of y:', len(y))

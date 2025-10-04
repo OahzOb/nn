0b0ded1488556d0e11c0755b2056734232015750
X99_win <X99_win.com>
2025-10-04 18:13:34 +0800

    added Network

--- nn.py
+++ nn.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import copy
 random.seed(42)
 
 
@@ -7,7 +8,6 @@ class Neuron:
     def __init__(self, weight: list[float, ], bias: float):
         self.weight = weight
         self.bias = bias
-
     def forward(self, x: list[float, ]) -> list[float]:
         y = 0
         for w, x0 in zip(self.weight, x):
@@ -49,9 +49,28 @@ class Layer:
         return output_activated
 
 
+class Network:
+    def __init__(self):
+        self.Layers = list()
+        self.Layers.append(Layer(input_size=2, output_size=64))
+        self.Layers.append(Layer(input_size=64, output_size=128))
+        self.Layers.append(Layer(input_size=128, output_size=1024))
+        self.Layers.append(Layer(input_size=1024, output_size=128))
+        self.Layers.append(Layer(input_size=128, output_size=32))
+    def forward(self, x: list[float, ]) -> list[float, ]:
+        if len(x) != self.Layers[0].input_size:
+            raise ValueError("Dims not matched!")
+        for layer in self.Layers:
+            y = layer.forward(x)
+            print(y)
+            x = copy.deepcopy(y)
+        output = y
+        return output
+
+
 if __name__ == '__main__':
-    layer = Layer(input_size=10, output_size=20)
-    x = [1] * 10
-    y = layer.forward(x=x)
+    net = Network()
+    x = [1] * 2
+    y = net.forward(x=x)
     print(y)
     print('len of y:', len(y))
